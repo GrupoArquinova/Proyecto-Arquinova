faa826f7ee1880ed38f52aa433ae8a43f9fa1f79
juangabriellondonoarias <juangabriellondonoarias@gmail.com>
2026-09-08 14:51:19 -0500

    first commit

---
+++ src/main/java/com/constructora_backend/ConstructoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.constructora_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructoraBackendApplication.class, args);
+	}
+
+}
